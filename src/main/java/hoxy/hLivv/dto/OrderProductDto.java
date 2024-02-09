22ceedf501f5442f3c2ce644e7648da2b0291455
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2024-02-10 02:26:09 +0900

    [FEAT] order product entity, repo, dto 생성

---
+++ src/main/java/hoxy/hLivv/dto/OrderProductDto.java
@@ -0,0 +1,12 @@
+package hoxy.hLivv.dto;
+
+import lombok.Data;
+
+@Data
+public class OrderProductDto {
+    private Long orderProductId;
+    private Long orderId;
+//    private Long productId;
+//    private Long collaboId;
+    private Long deliveryId;
+}
