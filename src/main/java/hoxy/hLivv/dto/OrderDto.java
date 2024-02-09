9020fb8a37f71f237ea9d512091aeb4551c75867
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2024-02-10 01:31:35 +0900

    [FEAT] Order Entity, dto, repo, enum 생성

---
+++ src/main/java/hoxy/hLivv/dto/OrderDto.java
@@ -0,0 +1,27 @@
+package hoxy.hLivv.dto;
+
+import hoxy.hLivv.entity.enums.OrderStatus;
+import lombok.*;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDto {
+    private Long orderId; // 주문번호
+    private Long memberId; // 멤버
+    private Long addressId; // 주소
+    private Date orderDate; // 주문일
+    private String requestMsg; //배송 요청 메세지
+    private Double subTotal; // 쿠폰, 할인 등 적용 전 금액
+    private Double orderTotal; // 최종 금액
+    private Double orderCash; // 현금 결제액
+    private Double orderPoint; // 포인트 결제액
+    private OrderStatus orderStatus; // 주문 상태
+    private Long orderCoupon; // 쿠폰 결제액
+    private Date requestDate; // 배송희망일
+}
---
+++ src/main/java/hoxy/hLivv/entity/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package hoxy.hLivv.entity.enums;
+
+public enum OrderStatus {
+    주문접수,
+    상품준비중,
+    배송중,
+    배송완료,
+    구매확정,
+    주문취소
+}
