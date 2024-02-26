0cf0b1f25e80e696271160081565da14108b17a3
이상원 <87797445+knowmetoowell@users.noreply.github.com>
2024-02-26 12:06:40 +0900

    Won (#15)
    * thymeleaf template init
    
    * [FEAT] Backoffice init
    
    * [FEAT] member signup 기능 변경
    
    * [FEAT] 백오피스 고객 조회 / 디자인 변경
    
    * [FEAT] Logout
    
    * [FEAT] Logout
    
    * [CHORE] logging level 변경
    
    * [FEAT] Exception Handle
    
    * [FEAT] Re-store CRUD
    
    Dto, entity, repo, enum, controller 정의
    
    * [CHORE] 시큐리티 설정 변경
    
    * [FEAT] Backoffice Restore init
    
    * [FEAT] Restore 이미지 변경 로직 추가
    
    * [FEAT] 고객조회, 상품조회 - 상세조회
    
    * [FEAT] 리스토어 기능 개발
    
    리스토어 정보 중 멤버, 프로덕트 확인 및 HTML/CSS/JS 수정
    
    * [FEAT] 리스토어 상세, 수정 등
    
    * [FEAT] 문자메세지 전송 API
    
    * Update .gitignore
    
    * Update chart-area-demo.js

--- src/main/resources/static/backoffice/js/demo/chart-area-demo.js
+++ src/main/resources/static/backoffice/js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 10000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+      data: [0, 15166, 18811, 30030, 37245, 84146, 111512, 48498, 59874, 69541, 98544, 90120],
     }],
   },
   options: {
